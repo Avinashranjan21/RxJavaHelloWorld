259e89b432c968a54b0907ce700d7d942fbe070d
Avinash Ranjan <avinash.ranjan21@gmail.com>
2017-11-13 22:29:14 +0530

    Initial commit

---
+++ app/src/main/java/com/bugfreetechnology/rxjavahelloworld/MainActivity.java
@@ -0,0 +1,55 @@
+package com.bugfreetechnology.rxjavahelloworld;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.hello_world_salute)
+    TextView helloWorldSalute;
+    @BindView(R.id.stock_updates_recycler_view)
+    RecyclerView recyclerView;
+    private LinearLayoutManager layoutManager;
+    private StockDataAdapter stockDataAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Observable.just("Hello Please use this app responsibly!")
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        helloWorldSalute.setText(s);
+                    }
+                });
+
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        stockDataAdapter = new StockDataAdapter();
+        recyclerView.setAdapter(stockDataAdapter);
+
+        Observable.just(new StockUpdate("GOOGLE", 12.43, new Date()),
+                new StockUpdate("APPLE", 645.1, new Date()),
+                new StockUpdate("TWITTER", 1.43, new Date()))
+                .subscribe(new Consumer<StockUpdate>() {
+                    @Override
+                    public void accept(StockUpdate stockSymbol) {
+                        stockDataAdapter.add(stockSymbol);
+                    }
+                });
+    }
+}
