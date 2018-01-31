32ae18c24ea2a251af13d27a55efe8ba6474a387
Avinash Ranjan <avinash.ranjan21@gmail.com>
2018-02-01 02:05:14 +0530

    different type observer added in MainActivity class

--- app/src/main/java/com/bugfreetechnology/rxjavahelloworld/MainActivity.java
+++ app/src/main/java/com/bugfreetechnology/rxjavahelloworld/MainActivity.java
@@ -12,7 +12,9 @@ import java.util.Date;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,12 +33,7 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         Observable.just("Hello Please use this app responsibly!")
-                .subscribe(new Consumer<String>() {
-                    @Override
-                    public void accept(String s) throws Exception {
-                        helloWorldSalute.setText(s);
-                    }
-                });
+                .subscribe(s->helloWorldSalute.setText(s));
 
         Observable.just("Hello from RetroLambda").subscribe(e -> Log.d(TAG, "Hello " + e));
 
@@ -51,5 +48,60 @@ public class MainActivity extends AppCompatActivity {
                 new StockUpdate("TWITTER", 1.43, new Date()))
                 .subscribe(stockSymbol -> stockDataAdapter.add(stockSymbol));
 
+        
+        
+//        Checking for Scheduler thread
+        
+        
+//         All the call made on Main Thread
+        Observable.just("First item", "Second item")
+                .doOnNext(e -> Log.d(TAG, "on-next:"    + Thread.currentThread().getName() + ":" + e))
+                .subscribe(e -> Log.d(TAG, "subscribe:" + Thread.currentThread().getName() + ":" + e));
+
+
+
+//        All call made on the RxCachedThread using subscribeOn method
+        Observable.just("First item", "Second item")
+                .subscribeOn(Schedulers.io())
+                .doOnNext(e -> Log.d(TAG, "on-next:" +
+                        Thread.currentThread().getName() + ":" + e))
+                .subscribe(e -> Log.d(TAG, "subscribe:" +
+                        Thread.currentThread().getName() + ":" + e));
+        
+//        The subscribe call will be made on Rx thread and observe will be done on Android Main Thread
+
+        Observable.just("First item", "Second item")
+                .subscribeOn(Schedulers.io())
+                .doOnNext(e -> Log.d(TAG, "on-next:" +
+                        Thread.currentThread().getName() + ":" + e))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(e -> Log.d(TAG, "subscribe:" +
+                        Thread.currentThread().getName() + ":" + e));
+        
+        
+        
+//        Now Checking the flow of RxJava using log statements
+
+        Observable.just("One", "Two")
+                .subscribeOn(Schedulers.io())
+                .doOnDispose(() -> log("doOnDispose"))
+                .doOnComplete(() -> log("doOnComplete"))
+                .doOnNext(e -> log("doOnNext", e))
+                .doOnEach(e -> log("doOnEach"))
+                .doOnSubscribe((e) -> log("doOnSubscribe"))
+                .doOnTerminate(() -> log("doOnTerminate"))
+                .doFinally(() -> log("doFinally"))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(e -> log("subscribe", e));
+    }
+
+
+    private void log(String stage, String item) {
+        Log.d(TAG, stage + ":" + Thread.currentThread().getName() + ":" +
+                item);
+    }
+
+    private void log(String stage) {
+        Log.d(TAG, stage + ":" + Thread.currentThread().getName());
     }
 }
